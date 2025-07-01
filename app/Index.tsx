ebda4e54cc232e89e369ec01b912f318b36ff2ca
GuillermoSOFT-LS <guillermolarasoft@gmail.com>
2025-07-01 15:27:03 -0400

    Se agregaron las fonts a tailwindcss

--- app/Index.tsx
+++ app/Index.tsx
@@ -1,12 +1,16 @@
 import {View,Text} from "react-native";
 import {StatusBar} from "expo-status-bar";
-import  './global.css'
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
 const Index = ()=> {
+
+    const insets = useSafeAreaInsets()
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
+        // style={{flex: 1, paddingBottom: insets.bottom, paddingTop: insets.top}}
+        <View style={{paddingTop: insets.top, paddingBottom: insets.bottom}} className='flex'>
             <StatusBar style='auto'/>
-            <Text className=' text-5xl text-blue-500'>Hello World!</Text>
+            <Text className=' text-5xl font-work-black text-blue-500'>Hello World!</Text>
         </View>
     )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,33 @@
-import {Slot} from "expo-router";
+import {Slot, SplashScreen} from "expo-router";
+import {useEffect} from "react";
 import './global.css'
+import {useFonts} from "expo-font";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+
+
+SplashScreen.preventAutoHideAsync()
 
 const RootLayout = () => {
-    return <Slot />
+   const [fontsloaded, error] = useFonts({
+        'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
+        'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
+        'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf'),
+    })
+
+
+    useEffect(() => {
+        if (error) throw error
+
+        if (fontsloaded) SplashScreen.hideAsync()
+    }, [fontsloaded, error]);
+
+   if (!fontsloaded && !error) return null;
+return (
+    <SafeAreaProvider>
+        <Slot />
+    </SafeAreaProvider>
+    )
+
 }
 
 export default RootLayout
