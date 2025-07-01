b4aecfccfa778e3d7efb52804f0153027211ad6f
GuillermoSOFT-LS <guillermolarasoft@gmail.com>
2025-07-01 12:56:44 -0400

    Integracion de Nativewind y sus configuraciones

--- app/Index.tsx
+++ app/Index.tsx
@@ -1,11 +1,12 @@
 import {View,Text} from "react-native";
 import {StatusBar} from "expo-status-bar";
+import  './global.css'
 
 const Index = ()=> {
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <StatusBar style='auto'/>
-            <Text style={{fontSize: 60}}>Hello World!</Text>
+            <Text className=' text-5xl text-blue-500'>Hello World!</Text>
         </View>
     )
 }
